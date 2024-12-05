78680ad0bfaec2ad35caa8c3154fb3fb7b87fc81
Fabiodssa <fabiosantos041818@gmail.com>
2024-12-05 17:38:11 -0300

    att

--- pequenas criaçoes/Media/src/main/java/company/media/Estudante.java
+++ pequenas criaçoes/Media/src/main/java/company/media/Estudante.java
@@ -6,37 +6,24 @@ public class Estudante {
     public double nota2;
     public double nota3;
     double media;
-    
+    double falta;
     public double media(){
         return media=(nota1+nota2+nota3)/3;
     }
-    
-    /*
-    public void Mediabaixa(){
+
+    public String toString(){ 
         if (media()>=6){
             System.out.println("APROVADO");
         } else{
-            if ((media()<6) && (media()>4)){
+            if ((media()<6) && (media()>=4)){
                 System.out.println("RECUMPERACAO");
-            }       else{
-                         System.out.println("REPROVADO");
-                    }
-          }
-        } 
-    */
-    
-    public String toString(){
-        
-        if (media()>=6){
-            System.out.println("APROVADO");
-        } else{
-            if ((media()<6) && (media()>4)){
-                System.out.println("RECUMPERACAO");
+                 falta=(media()-6)*-1;
             }       else{
                          System.out.println("REPROVADO");
                     }
           }
         return "Nome: "+ nome + 
-               "\nMedia: "+ String.format("%.2f",(nota1+nota2+nota3)/3);       
+               "\nMedia: "+ String.format("%.2f",(nota1+nota2+nota3)/3)+
+               "\nFalta: "+ String.format("%.2f", falta);
     }
 }
