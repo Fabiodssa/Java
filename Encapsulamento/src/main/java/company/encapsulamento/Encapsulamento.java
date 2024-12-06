8d4524057c17681e5bfde1f683a12c2eba8825f6
Fabiodssa <fabiosantos041818@gmail.com>
2024-12-05 23:12:43 -0300

    att

---
+++ Encapsulamento/src/main/java/company/encapsulamento/Encapsulamento.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package company.encapsulamento;
+
+/**
+ *
+ * @author Fábio
+ */
+public class Encapsulamento {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
